021f48274eada6c8431c909010d8c8090f534463
Mahdi Samanbari <mahdisamanbari.it@gmail.com>
2024-09-12 13:16:29 +0330

    Search Filter project done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Data from './MOCK_DATA.json'
 
 const App = () => {
+  const[searchTerm,setSearchTerm]=useState('')
   return (
-    <div>
+    <div className='App'>
+      <input type="text" placeholder='Search...'
+      onChange={(event)=>{
+        setSearchTerm(event.target.value)
+      }} />
+      {
+        Data.filter((item)=>{
+          if(item.first_name.toLowerCase().includes(searchTerm.toLowerCase())){
+            return item
+            
+          }
+        }).map((item,key)=>{
+          return(
+           <div className='user' key={key}>
+            <p>{item.first_name}</p>
+          </div>
+          )
+        }
+        )
+      }
       
     </div>
   );
 };
 
 export default App;
+//.map((item,key)=>{
+ // return(
+  //  <div className='user' key={key}>
+    // <p>{item.first_name}</p>
+   //</div>
+   //)
+ //}
+ //)
